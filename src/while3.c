352dcc4ff02e18b7a6ac0c640d3e8480202c2255
Marcelo Martins <marcelomartt@gmail.com>
2026-05-27 14:51:11 +0000

    Trabalhando com estrutura de repetição While

---
+++ src/while3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int i = 1;
+    while( i <= 50){
+        if( i%3 == 0){
+            printf("Este número %d é multiplo de 3\n" ,i);
+
+        }
+        i++;
+    }
+    return 0;
+}
